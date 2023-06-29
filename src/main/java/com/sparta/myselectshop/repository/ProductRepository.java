28816a1c2404e4a173f2541522c6faf71084b154
ImJcm <wjsckdals0909@naver.com>
2023-06-30 01:25:44 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -12,6 +12,21 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByUser(User user, Pageable pageable);
+    /*
+        select
+            p.id,
+            p.title as product_title,
+            pf.product_id as product_id,
+            pf.folder_id as folder_id
+        from product as p left join product_folder as pf
+            on p.id = pf.product_id
+        where p.user_id = ?
+            and
+            pf.folder_id = ?
+        order by p.id {desc or asc}
+            limit 0, 10;
 
+        //limit {시작 위치} {출력 갯수}
+    */
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
