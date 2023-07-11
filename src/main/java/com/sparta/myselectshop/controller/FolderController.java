d1e5ef3d63474d0655cbe4923249bf9b18c5962c
ImJcm <wjsckdals0909@naver.com>
2023-07-11 22:56:57 +0900

    @ExceptionHandler 적용

--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -3,11 +3,14 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.dto.FolderResponseDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.exception.RestApiException;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -49,4 +52,16 @@ public class FolderController {
                 userDetails.getUser()
         );
     }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
 }
